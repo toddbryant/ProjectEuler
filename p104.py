2e800720bba97a0ffa0950c18a04b33fa630ef3c
Todd <toddbryant@live.com>
2024-08-26 18:23:15 -0400

    cleanup + fixes for #104

--- p104.py
+++ p104.py
@@ -7,31 +7,22 @@
 # Uses modular arithmetic to compute the last nine digits exactly and
 # the golden ratio to compute just the first nine digits of each number.
 
-from math import log, sqrt
-
+DIGITS = set(str('123456789'))
 def is9Pandigital(n):
-   # the 2^k binary digit of digitFlags is 1 if we have observed digit k
-   digitFlags = 0
-   for ctr in range(0,10):
-      digitFlags = digitFlags | (1 << (n%10))
-      n = n/10
-   return digitFlags == 1023
+    return set(str(n)) == DIGITS
 
+GOLDEN_RATIO = (1+5**0.5)/2
 
-GOLDEN_RATIO = (1+sqrt(5))/2
+# Start at a 9-digit Fibonacci number
+back, back_old, k = 433494437, 267914296, 43
+front = back
 
-# Generate a big Fibonacci number so the golden ratio is a good approximation
-f, fOld, k = 1, 0, 1
-while f < 10**8:
-   f, fOld, k = fOld+f, f, k+1
-
+digits = 16
 # Now compute just the first and last nine digits of each Fibonacci number
-lastNine, lastNineOld, firstNine ,firstNineOld = f, fOld, f, fOld
-while not (is9Pandigital(lastNine) and is9Pandigital(int(firstNine))):
-   firstNine, firstNineOld = firstNine * GOLDEN_RATIO, firstNine
-   if firstNine>10**9:
-      firstNine = firstNine/10
-   lastNine, lastNineOld = (lastNine + lastNineOld) % 10**9, lastNine
-   k = k+1
+while not (is9Pandigital(back) and is9Pandigital(int(front / 10**(digits-9)))):
+   front *= GOLDEN_RATIO
+   if front>10**digits:
+      front //= 10
+   back, back_old, k = (back + back_old) % 1_000_000_000, back, k + 1
 
-print k
+print(k)
